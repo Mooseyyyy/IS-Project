da55c7cde186bd248034fc8ea0dcebe0b20dc149
Mooseyyyy <hoekb24@gmail.com>
2024-01-18 11:49:21 -0700

    Updated structure of testing

--- project.py
+++
@@ -1,9 +0,0 @@
-from rbm import RBM
-import numpy as np
-
-rbm1 = RBM(num_visible = 20, num_hidden = 2)
-
-training_data1 = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-rbm1.train(training_data1, max_epochs = 5000)
-
-print ("Done") 
---
+++ rowRBM.py
@@ -0,0 +1,19 @@
+from rbm import RBM
+import numpy as np
+
+# Initialization of RBM
+rbm1 = RBM(num_visible = 5, num_hidden = 3)
+
+# Data used
+training_data = np.array([[1,1,1,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]])
+visible_data = np.array([[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,1,1,1,1]])
+
+
+rbm1.train(training_data, max_epochs = 5000)
+print("Done training")
+
+print("HU activated from training data")
+print(rbm1.run_visible(training_data))
+
+print("HU activated from test data")
+print(rbm1.run_visible(visible_data))
