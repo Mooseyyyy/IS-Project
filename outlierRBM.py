6961d1c3c4a764ec2f23ea3d5ec08fd39f449831
Mooseyyyy <hoekb24@gmail.com>
2024-02-19 12:37:38 -0700

    Typo

---
+++ outlierRBM.py
@@ -0,0 +1,36 @@
+# Imports
+from rbm import RBM
+import numpy as np
+
+# Numpy print settings to see activated nodes better
+np.set_printoptions(linewidth=np.inf, formatter={'all': lambda x: " {:.0f} ".format(x)})
+
+# Initialization of RBM
+training_rbm = RBM(num_visible = 15, num_hidden = 8)
+
+# Data used
+training_data =np.array([[1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
+                         [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
+                         [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0],
+                         [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
+                         [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
+                         [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0],
+                         [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
+                         [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
+                         [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0],
+                         [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
+                         [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
+                         [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0],
+			 [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
+                         [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
+                         [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0]])
+
+np.random.shuffle(training_data)
+
+# Training the model
+training_rbm.train(training_data, max_epochs = 5000)
+print("Done training")
+
+# Generate data
+print("Daydream")
+print(training_rbm.daydream(5))
