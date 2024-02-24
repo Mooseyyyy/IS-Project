b738e5ad913d15b9836bf9da414f91e8f5c331c7
Mooseyyyy <hoekb24@gmail.com>
2024-02-23 20:00:42 -0700

    Added generating database

--- outlierRBM.py
+++ outlierRBM.py
@@ -1,80 +1,24 @@
 # Imports
 from rbm import RBM
 import numpy as np
+from util import generate
 
 # Numpy print settings to see activated nodes better
 np.set_printoptions(linewidth=np.inf, formatter={'all': lambda x: " {:.0f} ".format(x)})
 
 # Initialization of RBM
-training_rbm = RBM(num_visible = 15, num_hidden = 8)
+training_rbm = RBM(num_visible = 100, num_hidden = 30)
 
-# Data used
-spread_training_data =np.array([[1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
-                                [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
-                                [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0],
-                                [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
-                                [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
-                                [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0],
-                                [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
-                                [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
-                                [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0],
-                                [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
-                                [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
-                                [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0],
-			        [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
-                                [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
-                                [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0]])
+training_data = generate(60)
 
-# Data used
-error_training_data =np.array([[1,0,1,0,0,1,0,1,0,0,1,1,0,0,0],
-                                [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
-                                [0,1,0,1,0,1,0,0,1,0,1,0,0,1,0],
-                                [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
-                                [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
-                                [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0],
-                                [1,0,1,0,0,0,1,1,0,0,1,1,0,0,0],
-                                [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
-                                [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0],
-                                [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
-                                [0,0,0,0,1,0,1,0,0,1,0,0,1,0,1],
-                                [0,1,0,1,0,1,0,1,1,0,0,0,0,1,0],
-			        [1,0,1,0,0,0,0,1,0,0,1,1,0,0,0],
-                                [0,0,0,0,1,0,1,1,0,1,0,0,1,0,1],
-                                [0,1,0,1,0,1,0,0,1,0,0,0,0,1,0]])
-
-
-np.random.shuffle(error_training_data)
+np.random.shuffle(training_data)
 
 # Training the model
-training_rbm.train(error_training_data, max_epochs = 5000)
+training_rbm.train(training_data, max_epochs = 5000)
 print("Done training")
 
 # Generate data
 print("Daydream")
-print(training_rbm.daydream(5))
-print("---")
-print(training_rbm.daydream(5))
-print("---")
-print(training_rbm.daydream(5))
-print("---")
-print(training_rbm.daydream(5))
-print("---")
-print(training_rbm.daydream(5))
-#evaluate(training_rbm.daydream(5))
+array = training_rbm.daydream(30)
+print(array[29:30])
 
-# Function to evaluate generated outputs
-def evalate(input): 
-  gen = input[-1,:]
-  dp1 = np.array([1,0,1,0,0,0,0,1,0,0,1,1,0,0,0])
-  dp2 = np.array([0,0,0,0,1,0,1,0,0,1,0,0,1,0,1])
-  dp2 = np.array([0,1,0,1,0,1,0,0,1,0,0,0,0,1,0])
-
-def hammingDistance(input1, input2):
-  i = 0
-  count = 0
-
-  while(i < len(input1)):
-    if(input1[i] != input2[i]):
-      count += 1
-    i += 1
-  return count
